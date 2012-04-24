64b6459edd6ada8b4332191555a3de411ba6064d
John White <jonttyy@gmail.com>
2012-04-24 21:05:18 +0100

    Working with static routes

--- src/routr.js
+++ src/routr.js
@@ -1,9 +1,32 @@
 (function(exports){
 	"use strict";
-	var r = {};
+	var routes = {},
 
+	addRoute = function(route,callback){
+		routes[route] = callback;
+	},
+	run = function(route){
+		var i,reg, args;
+		for(i in routes){if(routes.hasOwnProperty(i)){
+			console.log('Route::',i);
+			reg = new RegExp(i);
+			args = route.match(reg);
+			if(args){
+				routes[i].apply({}, args);
+				break;
+			}
+		}}
+		routes = {};
+	};
 
-	exports.route = function(){
-
+	exports.route = function(route,callback){
+		//allow either just func or string and func arguments for adding a route
+		if( (typeof route === 'function') || typeof route === 'string' && typeof callback === 'function'){
+			return addRoute(route,callback);
+		}
+		//if just a string argument then run tests
+		if(typeof route === 'string' && typeof callback === 'undefined'){
+			run(route);
+		}
 	};
 }(window));
--- test/routr.test.js
+++ test/routr.test.js
@@ -8,4 +8,30 @@
 		ok(typeof route === 'function','It should be a globally available function');
 	});
 
+	test('It should work with a basic path',1,function(){
+		var spy = this.spy();
+		route('/user/jontyy',spy);
+		route('/user/jontyy/');
+		ok(spy.calledOnce,'Expected the callback to have been called exactly once');
+	});
+
+	test('It should only use a single route',2,function(){
+		var s1 = this.spy(),
+			s2 = this.spy();
+
+		route('/user/jontyy',s1);
+		route('/user',s2);
+		route('/user/jontyy/messages');
+		ok(s1.calledOnce,'Expected the first route to have been called');
+		ok(s2.callCount === 0,'Expected the second route to not have been called');
+	});
+
+	test('It should not use incorrect routes',1,function(){
+		var spy = this.spy();
+
+		route('/user/jontyy',spy);
+		route('/blog/post/12');
+		ok(spy.callCount === 0,'Expected the user route to have not been called.');
+	});
+
 }());
