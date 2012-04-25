3b69c2e19580873a4ab3012477217ba1b9205af3
John White <jonttyy@gmail.com>
2012-04-25 22:42:49 +0100

    Updated grunt file for v0.3.9

--- grunt.js
+++ grunt.js
@@ -1,43 +1,46 @@
-config.init({
-	meta: {
-		banner: '/*route.js - MIT licensed - https://github.com/Jontyy/route.js*/'
-	},
-	lint: {
-		files: ['src/*.js']
-	},
-	concat: {
-		'route.js': ['<banner>','src/*.js'],
-	},
-	min: {
-		'route.min.js': ['<banner>', 'route.js']
-	},
-	qunit : {
-		all : ['test/*.html']
-	},
-	watch: {
-		files: ['src/*.js','test/*.js'],
-		tasks: 'lint concat min qunit' 
-	},
-	uglify: {},
-	jshint: {
-		options: {
-			expr: true,
-			browser: true,
-			strict: true,
-			undef : true,
-			curly : true,
-			eqeqeq : true,
-			forin : true,
-			immed : true,
-			latedef : true,
-			noarg : true,
-			noempty:true,
-			regexp:true,
-			trailing : true,
-			node : true,
-			devel:true,
-			smarttabs : true
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		meta: {
+			banner: '/*route.js - MIT licensed - https://github.com/Jontyy/route.js*/'
+		},
+		lint: {
+			files: ['src/*.js']
+		},
+		concat: {
+			'route.js': ['<banner>', 'src/*.js'],
+		},
+		min: {
+			'route.min.js': ['<banner>', 'route.js']
+		},
+		qunit: {
+			all: ['test/*.html']
+		},
+		watch: {
+			files: ['src/*.js', 'test/*.js'],
+			tasks: 'lint concat min qunit'
+		},
+		uglify: {},
+		jshint: {
+			options: {
+				expr: true,
+				browser: true,
+				strict: true,
+				undef: true,
+				curly: true,
+				eqeqeq: true,
+				forin: true,
+				immed: true,
+				latedef: true,
+				noarg: true,
+				noempty: true,
+				regexp: true,
+				trailing: true,
+				node: true,
+				devel: true,
+				smarttabs: true
+			}
 		}
-	}
-});
-task.registerTask('default', 'lint concat min qunit');
+	});
+	grunt.registerTask('default', 'lint concat min qunit');
+};
